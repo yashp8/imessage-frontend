eb29d589a5f08ba2fe3349d797ebaa9ee4fac6f8
mowglie <Yash.Patel@yewtechnologies.co.uk>
2022-10-10 11:36:17 +0100

    create conversation

--- src/components/Chat/Coversations/ConversationList.tsx
+++ src/components/Chat/Coversations/ConversationList.tsx
@@ -1,40 +1,38 @@
 import * as React from 'react';
-import {Session} from "next-auth";
-import {Box, Text} from "@chakra-ui/react";
-import ConversationModel from './Model/Model'
-import {useState} from "react";
+import { Session } from 'next-auth';
+import { Box, Text } from '@chakra-ui/react';
+import ConversationModel from './Model/Model';
+import { useState } from 'react';
 
 interface IConversationListProps {
-    session: Session
+  session: Session;
 }
 
-const ConversationList: React.FunctionComponent<IConversationListProps> = (
-    {session},
-) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const onOpen = () => setIsOpen(true);
-    const onClose = () => setIsOpen(false);
+const ConversationList: React.FunctionComponent<IConversationListProps> = ({
+  session,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
 
-    return (
-        <Box width='100%'>
-            <Box
-                py={2}
-                px={4}
-                mb={4}
-                bg={"blackAlpha.300"}
-                borderRadius={4}
-                cursor='pointer'
-                onClick={onOpen}
-            >
-                <Text
-                    textAlign='center'
-                    color='whiteAlpha.800'
-                    fontWeight={500}
-                >Find or start conversation</Text>
-            </Box>
-            <ConversationModel isOpen={isOpen} onClose={onClose}/>
-        </Box>
-    );
+  return (
+    <Box width='100%'>
+      <Box
+        py={2}
+        px={4}
+        mb={4}
+        bg={'blackAlpha.300'}
+        borderRadius={4}
+        cursor='pointer'
+        onClick={onOpen}
+      >
+        <Text textAlign='center' color='whiteAlpha.800' fontWeight={500}>
+          Find or start conversation
+        </Text>
+      </Box>
+      <ConversationModel session={session} isOpen={isOpen} onClose={onClose} />
+    </Box>
+  );
 };
 
 export default ConversationList;
--- src/components/Chat/Coversations/Model/UserSearchList.tsx
+++ src/components/Chat/Coversations/Model/UserSearchList.tsx
@@ -4,40 +4,50 @@ import {Avatar, Button, Flex, Stack, Text} from "@chakra-ui/react";
 
 interface IUserSearchListProps {
     users: Array<SearchUser>
+    addParticipant: (user: SearchUser) => void;
 }
 
-const UserSearchList: React.FC<IUserSearchListProps> = ({users}) => {
-    return (
-        <div>
-            {users.length === 0 ? (
-                <Flex mt={6} justifyContent='center'>
-                    <Text>No users found</Text>
-                </Flex>
-            ) : (
-                <Stack mt={6}>
-                    {users.map(user => (
-                        <Stack
-                            key={user.id}
-                            direction='row'
-                            align='center'
-                            spacing={4}
-                            py={2}
-                            px={4}
-                            borderRadius={4}
-                            _hover={{bg: 'whiteAlpha.200'}}
-                        >
-                            <Avatar/>
-                            <Flex justify='space-between' align='center' width='100%'>
-                                <Text color='whiteAlpha.700'>{user.username}</Text>
-                                <Button bg='brand.100' _hover={{bg: 'brand.200'}} onClick={() => {
-                                }}>Select</Button>
-                            </Flex>
-                        </Stack>
-                    ))}
-                </Stack>
-            )}
-        </div>
-    );
-};
+const UserSearchList: React.FC<IUserSearchListProps> =
+    ({
+         users,
+         addParticipant
+     }) => {
+        return (
+            <div>
+                {users.length === 0 ? (
+                    <Flex mt={6} justifyContent='center'>
+                        <Text>No users found</Text>
+                    </Flex>
+                ) : (
+                    <Stack mt={6}>
+                        {users.map(user => (
+                            <Stack
+                                key={user.id}
+                                direction='row'
+                                align='center'
+                                spacing={4}
+                                py={2}
+                                px={4}
+                                borderRadius={4}
+                                _hover={{bg: 'whiteAlpha.200'}}
+                            >
+                                <Avatar/>
+                                <Flex justify='space-between' align='center' width='100%'>
+                                    <Text color='whiteAlpha.700'>{user.username}</Text>
+                                    <Button
+                                        bg='brand.100'
+                                        _hover={{bg: 'brand.200'}}
+                                        onClick={() =>
+                                            addParticipant(user)
+                                        }>Select
+                                    </Button>
+                                </Flex>
+                            </Stack>
+                        ))}
+                    </Stack>
+                )}
+            </div>
+        );
+    };
 
 export default UserSearchList;
