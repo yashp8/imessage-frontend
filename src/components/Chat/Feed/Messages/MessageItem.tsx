88638f74b26037b0c77c8ffcac989a45a322e5af
Yash Patel <Yash.Patel@yewtechnologies.co.uk>
2022-12-26 20:27:15 +0000

    add message view

---
+++ src/components/Chat/Feed/Messages/MessageItem.tsx
@@ -0,0 +1,78 @@
+import { Avatar, Box, Flex, Stack, Text } from '@chakra-ui/react';
+import { formatRelative } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
+
+interface MessageItemProps {
+  message: any;
+  sentByMe: boolean;
+}
+
+const formatRelativeLocale = {
+  lastWeek: `eeee 'at' p`,
+  yesterday: `'Yesterday at' p`,
+  today: `p`,
+  other: 'MM/dd/yy',
+};
+
+const MessageItem: React.FC<MessageItemProps> = ({ message, sentByMe }) => {
+  return (
+    <Stack
+      direction='row'
+      p={4}
+      spacing={4}
+      _hover={{ bg: 'whiteAlpha.200' }}
+      justify={sentByMe ? 'flex-end' : 'flex-start'}
+      wordBreak='break-word'
+      //   border='1px solid red '
+    >
+      {!sentByMe && (
+        <Flex align={'flex-end'}>
+          <Avatar size='sm' />
+        </Flex>
+      )}
+      <Stack spacing={1} width='100%'>
+        <Stack
+          direction='row'
+          align='center'
+          justify={sentByMe ? 'flex-end' : 'flex-start'}
+        >
+          {!sentByMe && (
+            <Text fontWeight={500} textAlign='left'>
+              {message.sender.username}
+            </Text>
+          )}
+          <Text fontSize={14} color='whitw.Alpha.700'>
+            {formatRelative(
+              // new Date(message.createdAt),
+              message.createdAt,
+              new Date(),
+              {
+                locale: {
+                  ...enUS,
+                  formatRelative: (token) =>
+                    formatRelativeLocale[
+                      token as keyof typeof formatRelativeLocale
+                    ],
+                },
+              },
+            )}
+          </Text>
+        </Stack>
+        <Flex justify={sentByMe ? 'flex-end' : 'flex-start'}>
+          <Box
+            bg={sentByMe ? 'brand.100' : 'whiteAlpha.300'}
+            px={2}
+            py={1}
+            borderRadius={12}
+            maxWidth='65%'
+          >
+            <Text>{message.body}</Text>
+          </Box>
+        </Flex>
+      </Stack>
+      {/* {message.body} */}
+    </Stack>
+  );
+};
+
+export default MessageItem;
